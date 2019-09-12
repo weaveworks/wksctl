7a826885161d509b13ac272dd5ee174ce7e5c924
Michał Flendrich <michal@flendrich.pro>
2019-09-12 07:14:12 +0200

    manifests: refactor: extract shared code to a new package `pkg/manifests`

--- cmd/wksctl/apply.go
+++ cmd/wksctl/apply.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"context"
-	"io/ioutil"
-	"os"
 	"path/filepath"
 
 	log "github.com/sirupsen/logrus"
@@ -11,13 +8,10 @@ import (
 	"github.com/weaveworks/wksctl/pkg/addons"
 	"github.com/weaveworks/wksctl/pkg/apis/wksprovider/machine/config"
 	wksos "github.com/weaveworks/wksctl/pkg/apis/wksprovider/machine/os"
+	"github.com/weaveworks/wksctl/pkg/manifests"
 	"github.com/weaveworks/wksctl/pkg/specs"
 	"github.com/weaveworks/wksctl/pkg/utilities/kubeadm"
 	"github.com/weaveworks/wksctl/pkg/utilities/manifest"
-	xcryptossh "golang.org/x/crypto/ssh"
-	gogit "gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/plumbing"
-	gogitssh "gopkg.in/src-d/go-git.v4/plumbing/transport/ssh"
 )
 
 // applyCmd represents the apply command
@@ -70,86 +64,7 @@ func applyRun(cmd *cobra.Command, args []string) {
 
 	cpath := filepath.Join(applyOptions.gitPath, applyOptions.clusterManifestPath)
 	mpath := filepath.Join(applyOptions.gitPath, applyOptions.machinesManifestPath)
-	initiateCluster(getManifests(cpath, mpath, applyOptions.gitURL, applyOptions.gitBranch, applyOptions.gitDeployKeyPath, applyOptions.gitPath))
-}
-
-func getManifests(clusterOption, machinesOption, gitURL, gitBranch, gitDeployKeyPath, gitPath string) (string, string, func()) {
-	clusterManifestPath := clusterOption
-	machinesManifestPath := machinesOption
-	var closer = func() {
-	}
-	if gitURL != "" {
-		clusterManifestPath, machinesManifestPath, closer = syncRepo(gitURL, gitBranch, gitDeployKeyPath, gitPath)
-		log.WithFields(log.Fields{"cluster": clusterManifestPath, "machines": machinesManifestPath}).Debug("manifests")
-	}
-	return clusterManifestPath, machinesManifestPath, closer
-}
-
-func cloneOptions(url, deployKeyPath, branch string) gogit.CloneOptions {
-	co := gogit.CloneOptions{
-		URL: url,
-	}
-	if branch != "" {
-		co.SingleBranch = true
-		co.ReferenceName = plumbing.NewBranchReferenceName(branch)
-	}
-	if deployKeyPath == "" {
-		return co
-	}
-	pem, err := ioutil.ReadFile(deployKeyPath)
-	if err != nil {
-		log.Fatalf("Failed to read deploy key %s - %v", deployKeyPath, err)
-	}
-	signer, err := xcryptossh.ParsePrivateKey(pem)
-	if err != nil {
-		log.Fatalf("Failed to parse private key - %v", err)
-	}
-
-	aith := &gogitssh.PublicKeys{User: "git", Signer: signer}
-	co.Auth = aith
-
-	return co
-
-}
-func syncRepo(url, branch, deployKeyPath, relativeRoot string) (string, string, func()) {
-	srcDir, err := ioutil.TempDir("", "wkp")
-	if err != nil {
-		log.Fatalf("Failed to create temp dir - %v", err)
-	}
-	closer := func() {
-		os.RemoveAll(srcDir)
-	}
-	lCtx := log.WithField("repo", url)
-	ctx := context.Background()
-	opt := cloneOptions(url, deployKeyPath, branch)
-	r, err := gogit.PlainCloneContext(ctx, srcDir, false, &opt)
-
-	if err != nil {
-		closer()
-		lCtx.Fatal(err)
-	}
-	lCtx.WithField("config", r.Config).Debug("cloned")
-
-	rootDir := filepath.Join(srcDir, relativeRoot)
-	files, err := ioutil.ReadDir(rootDir)
-	if err != nil {
-		closer()
-		lCtx.Fatalf("Failed to read directory %s - %v", rootDir, err)
-	}
-	var cYaml, mYaml string
-	for _, file := range files {
-		switch file.Name() {
-		case "cluster.yaml":
-			cYaml = filepath.Join(rootDir, file.Name())
-		case "machine.yaml", "machines.yaml":
-			mYaml = filepath.Join(rootDir, file.Name())
-		}
-	}
-	if cYaml == "" || mYaml == "" {
-		closer()
-		lCtx.WithField("repo", url).Fatal("Cluster and Machine yaml must be in repo")
-	}
-	return cYaml, mYaml, closer
+	initiateCluster(manifests.Get(cpath, mpath, applyOptions.gitURL, applyOptions.gitBranch, applyOptions.gitDeployKeyPath, applyOptions.gitPath))
 }
 
 func initiateCluster(clusterManifestPath, machinesManifestPath string, closer func()) {
