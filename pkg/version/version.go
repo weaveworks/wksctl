758c61b964ef75c5b8d1f3711da6df66e6ba8bd9
Michał Flendrich <michal@flendrich.pro>
2019-09-17 22:26:35 +0200

    version: set default ImageTag to 0.8.0-beta.1, improve the comment

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,9 +1,10 @@
 package version
 
 var (
-	// The constants below are to be set by flags passed to `go build`.
-	// Examples: -X version.Version=xxxxx -X version.ImageTag=yyyyy
+	// The constants below are to be overridden by linker flags passed to `go build`.
+	// Examples: -X github.com/weaveworks/wksctl/pkg/version.Version=xxxxx -X github.com/weaveworks/wksctl/pkg/version.ImageTag=yyyyy
+	// If wksctl is used as an imported module, the importing program shall override these values using the linker flags above. If not done, the defaults below will be used instead.
 
 	Version  = "undefined"
-	ImageTag = "latest"
+	ImageTag = "0.8.0-beta.1"
 )
