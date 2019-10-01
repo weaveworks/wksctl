8d29fa00ad0df861c6d7f6b5bab92820a9533317
Chanwit Kaewkasi <chanwit@gmail.com>
2019-10-01 17:36:14 +0700

    implement profile enabile and disable

---
+++ pkg/git/git.go
@@ -0,0 +1,76 @@
+package git
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+	giturls "github.com/whilp/git-urls"
+)
+
+func gitExec(args ...string) error {
+	cmd := exec.Command("git", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func HasNoStagedChanges() error {
+	return errors.Wrap(gitExec("diff", "--staged", "--exit-code"), "repository contains staged changes")
+}
+
+func RmRecursive(path string) error {
+	return gitExec("rm", "-r", "--", path)
+}
+
+func AddAll(path string) error {
+	return gitExec("add", "-A", path)
+}
+
+func SubtreeAdd(path, repository, revision string) error {
+	return gitExec("subtree", "add", "--squash", "--prefix", path, repository, revision)
+}
+
+func Commit(message string) error {
+	log.Infof("Committing the changes...")
+	err := gitExec("commit", "-m", message)
+	if err == nil {
+		log.Info("Committed the changes.")
+	}
+	return err
+}
+
+func Push() error {
+	log.Info("Pushing to the remote...")
+	err := gitExec("push", "-v", "origin", "HEAD")
+	if err == nil {
+		log.Info("Pushed successfully.")
+	}
+	return err
+}
+
+func HostAndRepoPath(repoURL string) (string, string, error) {
+	url, err := giturls.Parse(repoURL)
+	if err != nil {
+		return "", "", errors.Wrapf(err, "unable to parse git URL '%s'", repoURL)
+	}
+
+	return url.Hostname(), strings.TrimRight(url.Path, ".git"), nil
+}
+
+func IsGitURL(rawURL string) error {
+	parsedURL, err := giturls.Parse(rawURL)
+	if err != nil {
+		return err
+	}
+	if !parsedURL.IsAbs() {
+		return fmt.Errorf("URL %q does not have a scheme", parsedURL)
+	}
+	if parsedURL.Hostname() == "" {
+		return fmt.Errorf("URL %q has an empty hostname", parsedURL)
+	}
+	return nil
+}
