2b901128fbef2431415547d6b4fb29d0d895ac9d
Michał Flendrich <michal@flendrich.pro>
2019-10-03 23:45:34 +0200

    examples: remove sshKeyPath + docs

--- examples/gce/create-instances.sh
+++ examples/gce/create-instances.sh
@@ -10,8 +10,8 @@ ensure_ssh_key() {
     ssh-keygen -q -t rsa -b 4096 -C "wks-dev@weave.works" -f $name -N ""
 }
 
-ensure_ssh_key ssh_key
-echo "wks:$(cat ./ssh_key.pub | awk '{print $1" "$2" wks"}')" > ssh_key.list
+ensure_ssh_key cluster-key
+echo "wks:$(cat ./cluster-key.pub | awk '{print $1" "$2" wks"}')" > ssh_key.list
 
 gcloud compute --project="${project}" instances create ${user}-wks-{1,2,3} \
   --network="${network}" \
