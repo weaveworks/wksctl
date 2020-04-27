9b202a046283d2288363eed42b7449ee8192f9bb
Bryan Boreham <bjboreham@gmail.com>
2020-04-27 15:02:41 +0000

    BareMetal cluster controller updated to v1alpha3
    (renamed from 'actuator')

---
+++ pkg/apis/wksprovider/controller/wksctl/cluster_controller.go
@@ -0,0 +1,100 @@
+package wks
+
+import (
+	"context"
+	"errors"
+
+	log "github.com/sirupsen/logrus"
+	baremetalspecv1 "github.com/weaveworks/wksctl/pkg/baremetal/v1alpha3"
+	corev1 "k8s.io/api/core/v1"
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/record"
+	"sigs.k8s.io/cluster-api/util"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+)
+
+// ClusterReconciler is responsible for managing this cluster, and ensuring its
+// state converges towards its definition.
+type ClusterReconciler struct {
+	client        client.Client
+	eventRecorder record.EventRecorder
+}
+
+// Reconcile creates or updates the cluster.
+func (a *ClusterReconciler) Reconcile(req ctrl.Request) (_ ctrl.Result, reterr error) {
+	ctx := context.TODO() // upstream will add this eventually
+	contextLog := log.WithField("name", req.NamespacedName)
+
+	// request only contains the name of the object, so fetch it from the api-server
+	bmc := &baremetalspecv1.BareMetalCluster{}
+	err := a.client.Get(ctx, req.NamespacedName, bmc)
+	if err != nil {
+		if apierrs.IsNotFound(err) { // isn't there; give in
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	// Get Cluster via OwnerReferences
+	cluster, err := util.GetOwnerCluster(ctx, a.client, bmc.ObjectMeta)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	if cluster == nil {
+		contextLog.Info("Cluster Controller has not yet set ownerReferences")
+		return ctrl.Result{}, nil
+	}
+	contextLog = contextLog.WithField("cluster", cluster.Name)
+
+	if util.IsPaused(cluster, bmc) {
+		contextLog.Info("BareMetalCluster or linked Cluster is marked as paused. Won't reconcile")
+		return ctrl.Result{}, nil
+	}
+
+	// Object still there but with deletion timestamp => run our finalizer
+	if !bmc.ObjectMeta.DeletionTimestamp.IsZero() {
+		a.recordEvent(cluster, corev1.EventTypeNormal, "Delete", "Deleted cluster %v", cluster.Name)
+		return ctrl.Result{}, errors.New("ClusterReconciler#Delete not implemented")
+	}
+
+	// FIXME!  do we need any more?
+
+	return ctrl.Result{}, nil
+}
+
+func (r *ClusterReconciler) SetupWithManager(mgr ctrl.Manager, options controller.Options) error {
+	controller, err := ctrl.NewControllerManagedBy(mgr).
+		WithOptions(options).
+		For(&baremetalspecv1.BareMetalCluster{}).
+		WithEventFilter(pausedPredicates()).
+		Build(r)
+
+	if err != nil {
+		return err
+	}
+	_ = controller // not currently using it here, but it will run in the background
+	return nil
+}
+
+func (a *ClusterReconciler) recordEvent(object runtime.Object, eventType, reason, messageFmt string, args ...interface{}) {
+	a.eventRecorder.Eventf(object, eventType, reason, messageFmt, args...)
+	switch eventType {
+	case corev1.EventTypeWarning:
+		log.Warnf(messageFmt, args...)
+	case corev1.EventTypeNormal:
+		log.Infof(messageFmt, args...)
+	default:
+		log.Debugf(messageFmt, args...)
+	}
+}
+
+// NewClusterReconciler creates a new cluster reconciler.
+func NewClusterReconciler(client client.Client, eventRecorder record.EventRecorder) (*ClusterReconciler, error) {
+	return &ClusterReconciler{
+		client:        client,
+		eventRecorder: eventRecorder,
+	}, nil
+}
