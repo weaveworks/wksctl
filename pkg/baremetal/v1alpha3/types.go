a6e65182669b70a078cf7d969d248cb2e8d0e3c7
Bryan Boreham <bjboreham@gmail.com>
2020-04-30 14:23:18 +0000

    Enable controller-gen for baremetal types

--- pkg/baremetal/v1alpha3/types.go
+++ pkg/baremetal/v1alpha3/types.go
@@ -4,6 +4,9 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// +kubebuilder:object:generate=true
+// +groupName=cluster.weave.works
+
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type BareMetalCluster struct {
 	metav1.TypeMeta   `json:",inline"`
