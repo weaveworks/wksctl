9a79860748971a1b715fec1a85c89e6f8669f115
Dann Church <dann@weave.works>
2020-05-06 19:41:38 -0600

    print run object in test for debugging

--- test/integration/test/apply_test.go
+++ test/integration/test/apply_test.go
@@ -276,6 +276,7 @@ func testDebugLogging(t *testing.T, kubeconfig string) {
 
 	run, err := exe.RunV(kubectl,
 		fmt.Sprintf("--kubeconfig=%s", kubeconfig), "get", "pods", "-l", "name=wks-controller", "-A", "-o", "json")
+	fmt.Printf("run: %+v", run)
 	assert.NoError(t, err)
 	assert.Equal(t, 0, run.ExitCode())
 
