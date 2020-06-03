5a5e193d6ea7d41b2b3e51c6cd8a01bd4703d581
Sara El-Zayat <st.elzayat@gmail.com>
2020-06-03 21:00:29 +0200

    Fix number of masters in if condition

--- pkg/apis/wksprovider/controller/wksctl/machine_actuator.go
+++ pkg/apis/wksprovider/controller/wksctl/machine_actuator.go
@@ -377,7 +377,7 @@ func (a *MachineActuator) delete(ctx context.Context, cluster *clusterv1.Cluster
 	if err != nil {
 		return err
 	}
-	if isMaster && len(masters) > 1 {
+	if isMaster && len(masters) == 1 {
 		return gerrors.Wrapf(err, "there should be at least one master")
 	}
 	if err := drain.Drain(node, a.clientSet, drain.Params{
