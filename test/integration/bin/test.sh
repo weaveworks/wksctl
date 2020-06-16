31d8b74c81aa79e0aaf5d5737f4e68e505d69dad
Dennis Marttinen <dennis@weave.works>
2020-06-16 15:25:32 +0300

    Update binary paths in the integration tests
    Signed-off-by: Dennis Marttinen <dennis@weave.works>

--- test/integration/bin/test.sh
+++ test/integration/bin/test.sh
@@ -8,4 +8,4 @@ IMGTAG=$(./tools/image-tag)
 export PATH=$GOROOT/bin:$PATH
 # Work around for broken docker package in RHEL
 export DOCKER_VERSION='1.13.1-75*'
-go test -failfast -v -timeout 1h ./test/integration/test -args -run.interactive -cmd /tmp/workspace/cmd/wksctl/wksctl -tags.wks-k8s-krb5-server=$IMGTAG -tags.wks-mock-authz-server=$IMGTAG
+go test -failfast -v -timeout 1h ./test/integration/test -args -run.interactive -cmd /tmp/workspace/bin/wksctl -tags.wks-k8s-krb5-server=$IMGTAG -tags.wks-mock-authz-server=$IMGTAG
