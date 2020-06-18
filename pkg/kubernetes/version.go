32641edfd182f401ce601bac1b8b9377f9a7d38f
Yiannis <yiannis.triantafyllopoulos@gmail.com>
2020-06-18 14:14:16 +0300

    Add wildcard for the upper limit

--- pkg/kubernetes/version.go
+++ pkg/kubernetes/version.go
@@ -4,4 +4,4 @@ package kubernetes
 const DefaultVersion = "1.16.11"
 
 // DefaultVersionsRange is the default Kubernetes versions' range used by WKS to validate Kubernetes versions.
-const DefaultVersionsRange = ">=1.14.1 <=1.17.7"
+const DefaultVersionsRange = ">=1.14.1 <=1.17.x"
