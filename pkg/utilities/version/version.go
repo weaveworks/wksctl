434e01d14bb756ac307d39785271cde1c68bbdd8
Dennis Marttinen <dennis@weave.works>
2020-06-29 16:17:50 +0300

    Address review feedback, remove unnecessary function
    Signed-off-by: Dennis Marttinen <dennis@weave.works>

--- pkg/utilities/version/version.go
+++ pkg/utilities/version/version.go
@@ -1,12 +1,9 @@
 package version
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
-
 	"github.com/blang/semver"
 	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/util/version"
 )
 
 const (
@@ -34,16 +31,6 @@ func MatchesRange(version, versionsRange string) (bool, error) {
 	return r(v), nil
 }
 
-// MustMatchRange delegates to MatchesRange but panics on error instead of
-// returning the error.
-func MustMatchRange(version, versionsRange string) bool {
-	matches, err := MatchesRange(version, versionsRange)
-	if err != nil {
-		panic(err)
-	}
-	return matches
-}
-
 func Jump(nodeVersion, machineVersion string) (bool, error) {
 	nodemajor, nodeminor, _, err := parseVersion(nodeVersion)
 	if err != nil {
@@ -56,33 +43,22 @@ func Jump(nodeVersion, machineVersion string) (bool, error) {
 	return machinemajor == nodemajor && machineminor-nodeminor > 1, nil
 }
 
-func LessThan(v1, v2 string) (bool, error) {
-	v1major, v1minor, v1patch, err := parseVersion(v1)
+func LessThan(s1, s2 string) (bool, error) {
+	v1, err := version.ParseSemantic(s1)
 	if err != nil {
 		return false, err
 	}
-	v2major, v2minor, v2patch, err := parseVersion(v2)
+	v2, err := version.ParseSemantic(s2)
 	if err != nil {
 		return false, err
 	}
-	return (v1major < v2major) ||
-		(v1major == v2major && v1minor < v2minor) ||
-		(v1major == v2major && v1minor == v2minor && v1patch < v2patch), nil
+	return v1.LessThan(v2), nil
 }
 
-func parseVersion(v string) (int, int, int, error) {
-	v = strings.TrimPrefix(v, "v") // drop "v" at front
-	chunks := strings.Split(v, ".")
-	if len(chunks) != 3 { // major.minor.patch
-		return -1, -1, -1, fmt.Errorf("Invalid kubernetes version: %s", v)
+func parseVersion(s string) (int, int, int, error) {
+	v, err := version.ParseSemantic(s)
+	if err != nil {
+		return -1, -1, -1, errors.Wrap(err, "invalid kubernetes version")
 	}
-	var results = []int{-1, -1, -1}
-	for idx, item := range chunks {
-		val, err := strconv.Atoi(item)
-		if err != nil {
-			return -1, -1, -1, errors.Wrapf(err, "version is invalid")
-		}
-		results[idx] = val
-	}
-	return results[0], results[1], results[2], nil
+	return int(v.Major()), int(v.Minor()), int(v.Patch()), nil
 }
