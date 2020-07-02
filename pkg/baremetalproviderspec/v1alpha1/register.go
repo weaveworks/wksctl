a18b48d6fe603d9f141ab9d2f93b2fc73721ceff
Lucas Käldström <lucas@weave.works>
2020-07-02 15:38:55 +0300

    Add a register.go file to v1alpha1 types, and remove old deepcopy-gen argument

---
+++ pkg/baremetalproviderspec/v1alpha1/register.go
@@ -0,0 +1,28 @@
+package v1alpha1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+const GroupName = "baremetalproviderspec"
+
+var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: "v1alpha1"}
+
+var (
+	SchemeBuilder      runtime.SchemeBuilder
+	localSchemeBuilder = &SchemeBuilder
+	AddToScheme        = localSchemeBuilder.AddToScheme
+)
+
+func init() {
+	localSchemeBuilder.Register(addKnownTypes)
+}
+
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&BareMetalClusterProviderSpec{},
+		&BareMetalMachineProviderSpec{},
+	)
+	return nil
+}
