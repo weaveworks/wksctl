93c1761416940b365aba6829e7613958dab2e6ed
Mark Emeis <mark.emeis@weave.works>
2020-08-11 05:35:20 -0600

    Update the version of weave-net to 2.7.0
    Cherry-pick of 575b70c63f187544c1c317421d34715aad6dac76
    
    Co-authored-by: Simon Howe <footless@gmail.com>

--- environments/local-docker-registry/retag_push.sh
+++ environments/local-docker-registry/retag_push.sh
@@ -23,8 +23,8 @@ k8s.gcr.io/kube-proxy:v1.16.11
 k8s.gcr.io/pause:3.1
 k8s.gcr.io/etcd:3.3.15-0
 k8s.gcr.io/coredns:1.6.2
-docker.io/weaveworks/weave-npc:2.5.1
-docker.io/weaveworks/weave-kube:2.5.1
+docker.io/weaveworks/weave-npc:2.7.0
+docker.io/weaveworks/weave-kube:2.7.0
 )
 
 # Default host for the local Docker registry:
