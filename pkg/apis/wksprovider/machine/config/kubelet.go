36f206e5af6f5d8d935edb5922b23fee0a2edb37
Dennis Marttinen <dennis@weave.works>
2020-08-19 15:14:40 +0300

    Remove files/functionality moved to cluster-api-provider-existinginfra
    Signed-off-by: Dennis Marttinen <dennis@weave.works>

--- pkg/apis/wksprovider/machine/config/kubelet.go
+++
@@ -1,8 +0,0 @@
-package config
-
-// KubeletConfig groups all options & flags which need to be passed to kubelet.
-type KubeletConfig struct {
-	NodeIP         string
-	CloudProvider  string
-	ExtraArguments map[string]string
-}
