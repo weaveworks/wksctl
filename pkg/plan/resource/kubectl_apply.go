9269d8063334cc21e98f3201f640cab6fc4ef58c
Bryan Boreham <bjboreham@gmail.com>
2020-10-19 15:32:00 +0000

    Update for move in location of 'WriteFile()'
    Done in http://github.com/weaveworks/cluster-api-provider-existinginfra/pull/30

--- pkg/plan/resource/kubectl_apply.go
+++ pkg/plan/resource/kubectl_apply.go
@@ -9,9 +9,9 @@ import (
 
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
-	"github.com/weaveworks/cluster-api-provider-existinginfra/pkg/apis/wksprovider/machine/scripts"
 	"github.com/weaveworks/cluster-api-provider-existinginfra/pkg/plan"
 	"github.com/weaveworks/cluster-api-provider-existinginfra/pkg/plan/resource"
+	capeiresource "github.com/weaveworks/cluster-api-provider-existinginfra/pkg/plan/resource"
 	"github.com/weaveworks/cluster-api-provider-existinginfra/pkg/utilities/manifest"
 	"github.com/weaveworks/libgitops/pkg/serializer"
 )
@@ -94,7 +94,7 @@ func writeTempFile(ctx context.Context, r plan.Runner, c []byte, fname string) (
 	}
 	path := strings.Trim(pathDirty, "\n")
 
-	if err := scripts.WriteFile(ctx, c, path, 0660, r); err != nil {
+	if err := capeiresource.WriteFile(ctx, c, path, 0660, r); err != nil {
 		// Try to delete the temp file.
 		if _, rmErr := r.RunCommand(ctx, fmt.Sprintf("rm -vf %q", path), nil); rmErr != nil {
 			log.WithField("path", path).Errorf("failed to clean up the temp file: %v", rmErr)
