0e1d99dd30bb58b0307ffb1ee3517c09d35bf769
Jerry Jackson <jerry@weave.works>
2020-10-22 17:18:41 -0600

    working on fixing integration tests with new backend

--- environments/local-docker-registry/retag_push.sh
+++ environments/local-docker-registry/retag_push.sh
@@ -23,6 +23,13 @@ k8s.gcr.io/kube-proxy:v1.16.11
 k8s.gcr.io/pause:3.1
 k8s.gcr.io/etcd:3.3.15-0
 k8s.gcr.io/coredns:1.6.2
+k8s.gcr.io/kube-apiserver:v1.19.3
+k8s.gcr.io/kube-controller-manager:v1.19.3
+k8s.gcr.io/kube-scheduler:v1.19.3
+k8s.gcr.io/kube-proxy:v1.19.3
+k8s.gcr.io/pause:3.2
+k8s.gcr.io/etcd:3.4.13-0
+k8s.gcr.io/coredns:1.7.0
 docker.io/weaveworks/weave-npc:2.7.0
 docker.io/weaveworks/weave-kube:2.7.0
 )
