b9cb6dddd45bf8dbac6d74b7b214278cb97c5703
Jenkins <jenkins@nextiva.com>
2021-02-09 14:27:33 -0700

    use cluster api version number

--- pkg/kubernetes/version.go
+++
@@ -1,4 +0,0 @@
-package kubernetes
-
-// DefaultVersionsRange is the default Kubernetes versions' range used by WKS to validate Kubernetes versions.
-const DefaultVersionsRange = ">=1.16.1 <=1.20.x"
