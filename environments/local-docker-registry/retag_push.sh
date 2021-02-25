c986d3da3a640c4d464bf03d48d403544b7f9ad9
Mark Emeis <mark.emeis@weave.works>
2021-02-25 15:46:58 -0700

    Update test version of etcd to 3.4.3

--- environments/local-docker-registry/retag_push.sh
+++ environments/local-docker-registry/retag_push.sh
@@ -21,7 +21,7 @@ k8s.gcr.io/kube-controller-manager:v1.18.15
 k8s.gcr.io/kube-scheduler:v1.18.15
 k8s.gcr.io/kube-proxy:v1.18.15
 k8s.gcr.io/pause:3.2
-k8s.gcr.io/etcd:3.4.13-0
+k8s.gcr.io/etcd:3.4.3-0
 k8s.gcr.io/coredns:1.6.7
 k8s.gcr.io/kube-apiserver:v1.20.4
 k8s.gcr.io/kube-controller-manager:v1.20.4
